d960e2e920f7e440ea00e112e9d54f899752c470
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-05-12 15:19:02 -0300

    Final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const LanguageSelectorBtn = document.getElementById("LanguageSelector")
+const LanguageSelectorChevron = document.getElementById("ChevronLanguageSelector")
+
+let Chevrondown = false
+
+LanguageSelectorBtn.addEventListener("click", () => {
+  if (Chevrondown === false) {
+    LanguageSelectorChevron.classList.add("fa-rotate-90")
+    document.querySelector("#ptbr").style.display = "block";
+    document.querySelector("#en").style.display = "block";
+    Chevrondown = true
+  } else {
+    LanguageSelectorChevron.classList.remove("fa-rotate-90")
+    document.querySelector("#ptbr").style.display = "none";
+    document.querySelector("#en").style.display = "none";
+    Chevrondown = false
+  };
+});
+
+
+
+
