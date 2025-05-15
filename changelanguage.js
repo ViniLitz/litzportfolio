990cfd1316cab29baf1c32d2c2be77a1ee99d4f4
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-05-15 13:47:17 -0300

    a

--- changelanguage.js
+++ changelanguage.js
@@ -1,3 +1,26 @@
+(function () {
+  const alreadyRedirected = localStorage.getItem('lang_redirected');
+  const manualLang = new URLSearchParams(window.location.search).get('lang');
+  const preferredLang = localStorage.getItem('preferred_language');
+
+  if (manualLang) {
+    localStorage.setItem('preferred_language', manualLang);
+    return;
+  }
+
+  if (alreadyRedirected || preferredLang) return;
+
+  const userLang = navigator.language || navigator.userLanguage;
+
+  if (userLang.startsWith('pt')) {
+    localStorage.setItem('lang_redirected', 'true');
+    window.location.href = '/indexptbr.html';
+  } else {
+    localStorage.setItem('lang_redirected', 'true');
+    window.location.href = '/index.html';
+  }
+})();
+
 const userLang = navigator.language || navigator.userLanguage;
 if (userLang.startsWith('pt')) {
   window.location.href = 'indexpt-br.html';
