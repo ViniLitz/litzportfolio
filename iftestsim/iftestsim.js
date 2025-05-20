eb954bbff44d202ea3d8705f6db7ae4027f6f9e1
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-05-20 19:54:40 -0300

    if

--- iftestsim/iftestsim.js
+++ iftestsim/iftestsim.js
@@ -3,7 +3,7 @@ const FirstQuestion = document.querySelector("FirstQuestion");
 FirstQuestion.addEventListener("submit", (event) => {
     const FirstQuestionData = new FormData(FirstQuestion);
     let output = "";
-        for (const entry of data) {
+        for (const entry of FirstQuestionData) {
       output = `${output}${entry[0]}=${entry[1]}\r`;
     }
     console.log(output);
