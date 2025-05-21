a3eadaff5c061d8e77c18c9e89b639cdd6fbc756
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-05-20 21:17:59 -0300

    if

--- iftestsim/iftestsim.js
+++ iftestsim/iftestsim.js
@@ -1,12 +1,17 @@
+//Questões e suas respostas no seguinte formato: [Texto da questão, Opção A, Opção B, Opção C, Opção D, Opção correta] 
+
 let QuestionsAndAnswers = [
     ["Quem descobriu o Brasil?", "Pedro Álvares Cabral", "D. Pedro I", "José Bonifácio", "Silvio Santos", "a"], 
     ["Quanto é 2 + 2?", "3", "4", "22", "5", "b"]
-]
+];
+
+//Inserir as questões na página
 
 const FirstQuestionText = document.querySelector(".FirstQuestionText");
 FirstQuestionText.innerHTML = "1. " + QuestionsAndAnswers[0][0]
 
 //Confirmar a resposta
+
 const FirstQuestion = document.querySelector(".FirstQuestion");
 
 FirstQuestion.addEventListener("submit", (event) => {
