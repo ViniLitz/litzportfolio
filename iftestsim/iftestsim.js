040547a801494735d01456c3fff8df3a8ab0300d
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-05-20 21:13:43 -0300

    if

--- iftestsim/iftestsim.js
+++ iftestsim/iftestsim.js
@@ -1,20 +1,21 @@
 let QuestionsAndAnswers = [
-    ["question1", "a"], ["question2", "b"]
+    ["Quem descobriu o Brasil?", "Pedro Álvares Cabral", "D. Pedro I", "José Bonifácio", "Silvio Santos", "a"], 
+    ["Quanto é 2 + 2?", "3", "4", "22", "5", "b"]
 ]
 
 const FirstQuestionText = document.querySelector(".FirstQuestionText");
-FirstQuestionText.innerHTML = "1. " + "oi"
+FirstQuestionText.innerHTML = "1. " + QuestionsAndAnswers[0][0]
 
+//Confirmar a resposta
 const FirstQuestion = document.querySelector(".FirstQuestion");
 
 FirstQuestion.addEventListener("submit", (event) => {
     const FirstQuestionData = new FormData(FirstQuestion);
-    let output = "";
-        for (const entry of FirstQuestionData) {
-      output = `${entry[1]}`;
+    for (const entry of FirstQuestionData) {
+        let output = `${entry[1]}`;
     }
     console.log(output);
     event.preventDefault();
-  },
+    },
   false,
 );
