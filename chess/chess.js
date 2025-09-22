9b425acc219ed79b2461fc01a067b7a13be9cfc5
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-09-22 17:30:29 -0300

    js

--- chess/chess.js
+++ chess/chess.js
@@ -1,7 +1,7 @@
 const board = document.querySelector(".board");
 const ctx = board.getContext("2d");
 
-for (let index = 1; index < 16; index++) {
+for (let index = 1; index < 64; index++) {
     if (ctx.fillStyle === "white") {
         ctx.fillStyle = "black"
     } else {
@@ -13,7 +13,22 @@ for (let index = 1; index < 16; index++) {
             break;
         case index < 16:
             ctx.fillRect(100 * index, 200 * index, 100, 100);
-        default:
+        case index < 24:
+            ctx.fillRect(100 * index, 300 * index, 100, 100);
+            break;
+        case index < 32:
+            ctx.fillRect(100 * index, 400 * index, 100, 100);
+        case index < 40:
+            ctx.fillRect(100 * index, 500 * index, 100, 100);
+            break;
+        case index < 48:
+            ctx.fillRect(100 * index, 600 * index, 100, 100);
+        case index < 56:
+            ctx.fillRect(100 * index, 700 * index, 100, 100);
+            break;
+        case index < 64:
+            ctx.fillRect(100 * index, 800 * index, 100, 100);
+        default: console.log("Erro na formação do tabuleiro")
             break;
     }
     
