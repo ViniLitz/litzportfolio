9316512b36e6d75635ef621abf77b67da670ba32
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-09-22 17:23:37 -0300

    chess

--- chess/chess.js
+++ chess/chess.js
@@ -2,6 +2,10 @@ const board = document.querySelector(".board");
 const ctx = board.getContext("2d");
 
 for (let index = 0; index < 8; index++) {
-    ctx.fillStyle = "white";
+    if (ctx.fillStyle === "white") {
+        ctx.fillStyle = "black"
+    } else {
+        ctx.fillStyle = "white"
+    }
     ctx.fillRect(100 * index, 100 * index, 100, 100);
 }
