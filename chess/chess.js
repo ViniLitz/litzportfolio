65ab18919b6ae2886a06760df48cf615f5ea85a3
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-09-22 17:14:36 -0300

    chess

--- chess/chess.js
+++ chess/chess.js
@@ -1,6 +1,5 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.imageSmoothingEnabled = false;
+const board = document.getElementById("board");
+const ctx = board.getContext("2d");
 
 for (let index = 0; index < 8; index++) {
     ctx.fillStyle = "white";
