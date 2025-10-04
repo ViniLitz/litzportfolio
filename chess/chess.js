775f2dd70b5cc0c81e8e9e5e36a0ef6170eccc1f
ViniLitz <117537357+ViniLitz@users.noreply.github.com>
2025-10-04 17:36:29 -0300

    chess

--- chess/chess.js
+++ chess/chess.js
@@ -1,77 +1,80 @@
-const board = document.querySelector(".board");
-const ctx = board.getContext("2d");
+let ColorOfSquare = "White"
 
-let BoardIndex = 1
+for (let BoardIndex = 0; BoardIndex < 8; BoardIndex++) {
+  const square = document.createElement("button")
+  if (ColorOfSquare === "White") {
+    square.classList.add("WhiteSquare")
+    ColorOfSquare = "Black"
+  } else {
+    square.classList.add("BlackSquare")
+    ColorOfSquare = "White"
+  }
+  document.body.appendChild(square)
+}
 
-for (BoardIndex = 1;BoardIndex  < 64; BoardIndex++) {
-    if (ctx.fillStyle === "white") {
-        ctx.fillStyle = "black"
-    } else {
-        ctx.fillStyle = "white"
-    }
-    switch (BoardIndex) {
-        case BoardIndex <= 8 || BoardIndex >= 1:
-            ctx.fillRect(100 * BoardIndex, 100 * BoardIndex, 100, 100);
-            break;
-        case BoardIndex <= 16 || BoardIndex >= 9:
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-            ctx.fillRect(100 * BoardIndex, 200 * BoardIndex, 100, 100);
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-        case BoardIndex <= 24 || BoardIndex >= 17:
-            ctx.fillRect(100 * BoardIndex, 300 * BoardIndex, 100, 100);
-            break;
-        case BoardIndex <= 32 || BoardIndex >= 25:
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-            ctx.fillRect(100 * BoardIndex, 400 * BoardIndex, 100, 100);
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-        case BoardIndex <= 40 || BoardIndex >= 33:
-            ctx.fillRect(100 * BoardIndex, 500 * BoardIndex, 100, 100);
-            break;
-        case BoardIndex <= 48 || BoardIndex >= 41:
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-            ctx.fillRect(100 * BoardIndex, 600 * BoardIndex, 100, 100);
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-        case BoardIndex <= 56 || BoardIndex >= 49:
-            ctx.fillRect(100 * BoardIndex, 700 * BoardIndex, 100, 100);
-            break;
-        case BoardIndex <= 64 || BoardIndex >= 57:
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-            ctx.fillRect(100 * BoardIndex, 800 * BoardIndex, 100, 100);
-            if (ctx.fillStyle === "white") {
-                ctx.fillStyle = "black"
-            } else {
-                ctx.fillStyle = "white"
-            }       
-        default: console.log("Erro na formação do tabuleiro")
-            break;
-    }
-    
+ColorOfSquare = "Black"
+const br1 = document.createElement("br")
+document.body.appendChild(br1)
+
+for (let BoardIndex = 0; BoardIndex < 8; BoardIndex++) {
+  const square = document.createElement("button")
+  if (ColorOfSquare === "White") {
+    square.classList.add("WhiteSquare")
+    ColorOfSquare = "Black"
+  } else {
+    square.classList.add("BlackSquare")
+    ColorOfSquare = "White"
+  }
+  document.body.appendChild(square)
+}
+
+ColorOfSquare = "White"
+const br2 = document.createElement("br")
+document.body.appendChild(br2)
+
+
+for (let BoardIndex = 0; BoardIndex < 8; BoardIndex++) {
+  const square = document.createElement("button")
+  if (ColorOfSquare === "White") {
+    square.classList.add("WhiteSquare")
+    ColorOfSquare = "Black"
+  } else {
+    square.classList.add("BlackSquare")
+    ColorOfSquare = "White"
+  }
+  document.body.appendChild(square)
+}
+
+ColorOfSquare = "Black"
+const br3 = document.createElement("br")
+document.body.appendChild(br3)
+
+
+for (let BoardIndex = 0; BoardIndex < 8; BoardIndex++) {
+  const square = document.createElement("button")
+  if (ColorOfSquare === "White") {
+    square.classList.add("WhiteSquare")
+    ColorOfSquare = "Black"
+  } else {
+    square.classList.add("BlackSquare")
+    ColorOfSquare = "White"
+  }
+  document.body.appendChild(square)
+}
+
+ColorOfSquare = "White"
+const br4 = document.createElement("br")
+document.body.appendChild(br4)
+
+
+for (let BoardIndex = 0; BoardIndex < 8; BoardIndex++) {
+  const square = document.createElement("button")
+  if (ColorOfSquare === "White") {
+    square.classList.add("WhiteSquare")
+    ColorOfSquare = "Black"
+  } else {
+    square.classList.add("BlackSquare")
+    ColorOfSquare = "White"
+  }
+  document.body.appendChild(square)
 }
